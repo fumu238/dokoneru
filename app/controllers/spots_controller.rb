b4e764b48a4cc84c1e3c9ac0d05cda95a1db5bcf
fumu238 <fumu238@gmail.com>
2018-08-03 12:55:03 +0000

    views

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -85,7 +85,7 @@ class SpotsController < ApplicationController
 		    end
 		  end
 		@q = Spot.ransack(params[:q])
-		@spots = @q.result(distinct: true).page(params[:page]).per(3)
+		@spots = @q.result(distinct: true).page(params[:page]).per(10)
 	end
 
 	private
